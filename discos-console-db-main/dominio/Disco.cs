907bd11ad4feb2a07dc5a5ff3a28eb96e5ef0a35
Federico 'Magus' Rocca <frocca17@gmail.com>
2025-11-16 17:16:37 -0300

    Mostrar un listado de discos en una vista Razor usando DiscoNegocio Agregar la opción de "Discos" al menú de la app.

--- discos-console-db-main/dominio/Disco.cs
+++ discos-console-db-main/dominio/Disco.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,10 +10,20 @@ namespace dominio
     public class Disco
     {
         public int Id { get; set; }
+        [Required]
+        [Display(Name = "Título")]
         public string Titulo { get; set; }
+        [Required]
+        [Display(Name = "Fecha de lanzamiento")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime FechaLanzamiento { get; set; }
+        [Required]
+        [Display(Name = "Canciones")]
         public int CantidadCanciones { get; set; }
+        [Required]
+        [Display(Name = "Tapa del disco")]
         public string UrlTapa { get; set; }
+        [Required]
         public Estilo Estilo { get; set; }
         public TipoEdicion TipoEdicion { get; set; }
     }
--- discos-console-db-main/dominio/Estilo.cs
+++ discos-console-db-main/dominio/Estilo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,8 @@ namespace dominio
     public class Estilo
     {
         public int Id { get; set; }
+        [Required]
+        [Display(Name = "Estilo")]
         public string Descripcion { get; set; }
     }
 }
